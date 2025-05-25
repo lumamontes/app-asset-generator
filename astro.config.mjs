fbb957145fc5a43e4c3cbd63600c7bbe08e09111
lumamontes <lumagoesmontes@gmail.com>
2025-05-25 01:03:56 -0300

    update configuration and enhance layout for improved performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,18 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: 'https://app-asset-generator.netlify.app',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+      minify: true,
+    },
+    optimizeDeps: {
+      include: ['react', 'react-dom'],
+    },
+  },
 });
